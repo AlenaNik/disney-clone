8fa79e97d9cce9414ee116853ae35c7ff7d4c499
AlenaNik <alena.nik.digital@gmail.com>
2019-10-26 12:53:38 +0200

    before interaction

---
+++ src/views/Home.vue
@@ -0,0 +1,107 @@
+<template>
+  <div class="my-8">
+    <div class="mb-12">
+      <img src="../assets/the_mandalorian.jpg" alt="" class="rounded">
+    </div>
+
+    <div class="categories flex items-center mb-12 -mx-4">
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4
+        hover:bg-gray-900 border border-gray-700 shadow-custom">
+          <img src="../assets/disney_logo.svg" alt="disney_logo">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4
+        hover:bg-gray-900 border border-gray-700 shadow-custom">
+          <img src="../assets/pixar_logo.svg" alt="disney_logo">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4
+        hover:bg-gray-900 border border-gray-700 shadow-custom">
+          <img src="../assets/marvel_logo.svg" alt="disney_logo">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4
+        hover:bg-gray-900 border border-gray-700 shadow-custom">
+          <img src="../assets/star_wars_logo.svg" alt="disney_logo">
+        </a>
+      </div>
+      <div class="w-1/5 px-4">
+        <a href="#" class="bg-gray-800 h-32 flex justify-center items-center rounded-lg p-4
+        hover:bg-gray-900 border border-gray-700 shadow-custom">
+          <img src="../assets/national_geographic_logo.svg" alt="disney_logo">
+        </a>
+      </div>
+    </div>
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Recommended For You</h2>
+      <div class="flex items-center">
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/star_wars_a_new_hope.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/snow_white.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/black_panther.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/zootopia.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/one_strange_rock.jpg" alt="starwars">
+          </a>
+        </div>
+      </div>
+    </div>
+    <div class="recommended px-6 mb-12">
+      <h2 class="text-gray-500 mb-1">Continue watching</h2>
+      <div class="flex items-center">
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/zootopia.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/one_strange_rock.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/the_sound_of_music.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/cinderella.jpg" alt="starwars">
+          </a>
+        </div>
+        <div class="shadow-xl mr-6">
+          <a href="#">
+            <img src="../assets/avengers_endgame.jpg" alt="starwars">
+          </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: "home",
+};
+</script>
